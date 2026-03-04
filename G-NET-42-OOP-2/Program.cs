68885ed485b77be339be08549a0d72c43d00a314
fayro <fayro@FAYROUZGOUMAA>
2026-03-04 23:53:47 +0200

    Q1,Q2

--- G-NET-42-OOP-2/Program.cs
+++ G-NET-42-OOP-2/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace G_NET_42_OOP_2
+﻿using System.ComponentModel;
+using System.Runtime.Intrinsics.X86;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace G_NET_42_OOP_2
 {
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double Area
+        {
+            get { return Width * Height; }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Q1
+            //a-
+            //    Public fields
+            //    No validation in Withdraw
+            //b- How to fix it
+            //    Make fields private
+            //     Use properties
+            //    Add validation in Withdraw
+            //c) Why Public Fields Are Bad Practice
+            //    Breaks encapsulation
+            //    Breaks encapsulation
+            //    Hard to maintain
 
+            #endregion
+            #region Q2
+            //-Field
+            //    Variable inside class
+            //    -Stores data
+            //    Usually private
+            //    No built-in validation
+            //-Property
+            //    Provides controlled access to field
+            //    Uses get and set
+            //    Can contain logic
+            //Can a Property Contain Logic ?
+            //         Yes.
+            Rectangle r = new Rectangle { Width = 5, Height = 4 };
+            Console.WriteLine(r.Area);  // 20
+            #endregion
         }
     }
 }
